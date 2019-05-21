97ded66f045382d7ab8009843925e6049bd2cf00
Maksim Larionov <maksim_larionov@mail.ru>
2019-05-21 23:18:51 +0700

    finplan2

--- FinansPlan/MainWindow.xaml.cs
+++ FinansPlan/MainWindow.xaml.cs
@@ -28,20 +28,21 @@ namespace FinansPlan
             ac.AddTran(DateTime.Parse("17.08.2018"), -149000, 1,TranCat.payCard);            
             ac.AddTran(DateTime.Parse("19.08.2018"), -16661.43, 1,TranCat.payCard);
             ac.AddTran(DateTime.Parse("19.08.2018"), -13700, 1,TranCat.getCash);
-            //ac.AddTran(DateTime.Parse("15.09.2018"), 12000, 1,TranCat.payCard);
+            ac.AddTran(DateTime.Parse("6.09.2018"), -11000, 1,TranCat.payCard);
+            ac.AddTran(DateTime.Parse("15.09.2018"), -1901.63, 1,TranCat.payCard);
             ac.Recalc();
-            var dat = DateTime.Parse("20.08.2018");//DateTime.Today
+            var dat = DateTime.Parse("22.09.2018");//DateTime.Today
             var total = ac.GetTotal(dat);
             var gett = ac.GetMaxCash(dat, true);
             textBlock1.Text = $"Total: {total}  Available:{gett} / {ac.GetMaxCash(dat, false)} ";
             listBox1.Items.Clear();
             foreach (var t in ac.Transactions.trans) listBox1.Items.Add(t);
-
+            /*
             var uni = new UnicreditAutoCredit();//DateTime.Parse("9.08.2018"), 233640.32, 24,11.3) 
             uni.Recalc();
             listBox1.Items.Clear();            
             foreach (var t in uni.Transactions.trans) listBox1.Items.Add(t);
-            textBlock1.Text = $"Total: {uni.Transactions.trans.Sum(pp=>pp.sum)}";
+            textBlock1.Text = $"Total: {uni.Transactions.trans.Sum(pp=>pp.sum)}";*/
         }
     }
 }
